25c41a2895c9489faecefec8042700f7a985e576
example <alisayar0107@gmail.com>
2020-12-24 18:39:38 +0300

    Template(handlebars) implemented, with a form that adds user to the json.

--- index.js
+++ index.js
@@ -1,18 +1,30 @@
 const express = require('express');
 const path = require('path');
-const logger = require('./middleware/logger')
+const exphbs = require('express-handlebars');
+const logger = require('./middleware/logger');
+const members = require('./Members');
 
 const app = express();
 
 // Init middleware
 // app.use(logger);
 
+// Handlebars Middleware
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
 // Body Parser Middleware
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
-// Set a static folder
-app.use(express.static(path.join(__dirname, 'public')));
+// Homepage route
+app.get('/', (req, res) => res.render('index', {
+    title: 'Member App',
+    members
+}));
+
+// // Set a static folder
+// app.use(express.static(path.join(__dirname, 'public')));
 
 // Members API Routes
 app.use('/api/members', require('./routes/api/members'))
